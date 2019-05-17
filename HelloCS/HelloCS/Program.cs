7cf18cbe69e44af4e14a3741a041ca45570021db
Huttman Balázs <huba95@hotmail.com>
2019-05-17 23:30:35 +0200

    Hello created

---
+++ HelloCS/HelloCS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
